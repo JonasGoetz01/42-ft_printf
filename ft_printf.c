80ce9f03cd66b9ec96eb55d2a70ae9f39ff9ef8b
Jonas Götz <jgotz@student.42heilbronn.de>
2023-10-12 16:17:08 +0200

    update printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,14 +6,22 @@
 /*   By: jgotz <jgotz@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 19:50:26 by jgotz             #+#    #+#             */
-/*   Updated: 2023/10/11 13:57:06 by jgotz            ###   ########.fr       */
+/*   Updated: 2023/10/12 15:06:06 by jgotz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int	is_dynamic_memory(void *ptr)
+{
+	return (ptr > (void *)0x1000 && ptr < (void *)0x100000000);
+}
+
 static void	ft_switch_format(va_list args, const char c, int *ret)
 {
+	char			*str;
+	unsigned int	i;
+
 	if (c == 'c')
 	{
 		ft_putchar_fd(va_arg(args, int), 1);
@@ -21,31 +29,41 @@ static void	ft_switch_format(va_list args, const char c, int *ret)
 	}
 	else if (c == 's')
 	{
-		ft_putstr_fd(va_arg(args, char *), 1);
-		*ret += (ft_strlen(va_arg(args, char *)));
+		str = va_arg(args, char *);
+		if (str == NULL)
+			return ;
+		ft_putstr_fd(str, 1);
+		*ret += (ft_strlen(str));
+		if (is_dynamic_memory(str))
+		{
+			free(str);
+		}
 	}
 	else if (c == 'p')
 	{
-		*ret += (ft_print_ptr(va_arg(args, unsigned long long)));
+		ft_putchar_fd('0', 1);
+		ft_putchar_fd('x', 1);
+		*ret += (ft_print_hex(va_arg(args, unsigned long), 'x')) + 2;
 	}
 	else if (c == 'd' || c == 'i')
 	{
-		ft_putstr_fd(ft_itoa(va_arg(args, int)), 1);
-		*ret += (ft_strlen(ft_itoa(va_arg(args, int))));
+		str = ft_itoa(va_arg(args, int));
+		ft_putstr_fd(str, 1);
+		*ret += (ft_strlen(str));
+		free(str);
 	}
 	else if (c == 'u')
 	{
-		*ret += (ft_print_udec(va_arg(args, int)));
+		i = va_arg(args, unsigned int);
+		ft_print_udec(i);
+		*ret += get_num_udigits(i);
 	}
 	else if (c == 'x' || c == 'X')
-	{
-		if (c == 'x')
-			*ret += (ft_print_lhex(va_arg(args, int)));
-		else if (c == 'X')
-			*ret += (ft_print_uhex(va_arg(args, int)));
-	}
+		*ret += (ft_print_hex(va_arg(args, unsigned long), c));
 	else if (c == '%')
 	{
+		ft_putchar_fd('%', 1);
+		*ret += 1;
 	}
 }
 
