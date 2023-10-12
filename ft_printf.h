202003496a0406de6971ef33464c38e37f0bb402
Jonas Götz <jgotz@student.42heilbronn.de>
2023-10-12 16:17:52 +0200

    update function signature

--- ft_printf.h
+++ ft_printf.h
@@ -17,13 +17,12 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-int	ft_printf(const char *str, ...);
-int	ft_print_uhex(int n);
-int	ft_print_udec(int n);
-int	ft_print_ptr(unsigned long long n);
-int	ft_print_percent(void);
-int	ft_print_lhex(int n);
-int	ft_print_int(int n);
-int	ft_print_dec(int n);
+int		ft_printf(const char *str, ...);
+void	ft_print_udec(unsigned int n);
+int		ft_print_percent(void);
+int		ft_print_int(int n);
+int		ft_print_dec(int n);
+int		get_num_udigits(unsigned int n);
+int		ft_print_hex(unsigned int n, char format);
 
 #endif
