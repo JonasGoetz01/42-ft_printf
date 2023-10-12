07d8c553e31473b7d15350b50f3102c0bfdd9157
Jonas Götz <jgotz@student.42heilbronn.de>
2023-10-12 16:16:57 +0200

    print unsigned dec

--- ft_print_udec.c
+++ ft_print_udec.c
@@ -6,13 +6,33 @@
 /*   By: jgotz <jgotz@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 19:50:26 by jgotz             #+#    #+#             */
-/*   Updated: 2023/10/11 13:57:06 by jgotz            ###   ########.fr       */
+/*   Updated: 2023/10/12 10:21:54 by jgotz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_print_udec(int n)
+int	get_num_udigits(unsigned int n)
 {
-	return (n);
+	int	i;
+
+	i = 0;
+	if (n == 0)
+		return (1);
+	while (n > 0)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
+}
+
+void	ft_print_udec(unsigned int n)
+{
+	char digit;
+
+	if (n > 9)
+		ft_print_udec(n / 10);
+	digit = '0' + n % 10;
+	write(1, &digit, 1);
 }
